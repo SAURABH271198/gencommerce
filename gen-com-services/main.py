f3b73562f8c4cc8ab81c7ffe5c795cf42a3f5e6e
SAURABH271198 <saurabhtaragi30@gmail.com>
2025-02-20 19:23:17 +0530

    added spaces

--- gen-com-services/main.py
+++ gen-com-services/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to GenCommerce AI backend"}
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
